4b81be640fe6d4cb87098eb5bbcab89fe7099d4f
Diego Romário <diego.romario@outlook.com>
2021-08-12 09:36:32 -0300

    Ajustando program

--- src/M4.WebApi/Program.cs
+++ src/M4.WebApi/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace M4.WebApi
@@ -8,13 +7,7 @@ namespace M4.WebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).ConfigureAppConfiguration((hostContext, builder) =>
-            {
-                if (hostContext.HostingEnvironment.IsDevelopment())
-                {
-                    builder.AddUserSecrets<Program>();
-                }
-            }).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
