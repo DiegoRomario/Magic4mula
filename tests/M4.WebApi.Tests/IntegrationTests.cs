9fad46751d98bc45442b4e01562ac205189473e5
Diego Romário <diego.romario@outlook.com>
2021-08-23 21:25:23 -0300

    Adicionando teste para login de usuário

--- tests/M4.WebApi.Tests/IntegrationTests.cs
+++ tests/M4.WebApi.Tests/IntegrationTests.cs
@@ -16,6 +16,9 @@ using System.Linq;
 using System.Net.Http.Json;
 using M4.Infrastructure.Data.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Net.Http;
+using System;
+
 namespace M4.WebApi.Tests
 {
     public class IntegrationTests : IClassFixture<M4Factory<StartupTesting>>
@@ -23,7 +26,8 @@ namespace M4.WebApi.Tests
         private const string APP_SETTINGS_TESTING_PATH = "Config/appsettings.Testing.json";
         private const string SOLUTION_RELATIVE_PATH = "src/M4.WebApi";
         private readonly WebApplicationFactory<StartupTesting> _factory;
-        public UserIdentityDbContext context { get; set; }
+        private UserIdentityDbContext _context { get; set; }
+        private HttpClient _client;
 
         public IntegrationTests(M4Factory<StartupTesting> factory)
         {
@@ -41,9 +45,9 @@ namespace M4.WebApi.Tests
                         .AddUserSecrets<IntegrationTests>()
                         .AddEnvironmentVariables();
 
-                    var teste = conf.Build()["ConnectionStrings:MagicFormulaSQLServer"];
-                    optionsBuilder.UseSqlServer(teste);
-                    this.context = new UserIdentityDbContext(optionsBuilder.Options);
+                    var connectionString = conf.Build()["ConnectionStrings:MagicFormulaSQLServer"];
+                    optionsBuilder.UseSqlServer(connectionString);
+                    this._context = new UserIdentityDbContext(optionsBuilder.Options);
                 });
 
                 builder.ConfigureTestServices(services =>
@@ -51,6 +55,16 @@ namespace M4.WebApi.Tests
                     services.AddMvc().AddApplicationPart(typeof(Startup).Assembly);
                 });
             });
+
+            var clientOptions = new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = true,
+                BaseAddress = new Uri("http://localhost/api/"),
+                HandleCookies = true,
+                MaxAutomaticRedirections = 7
+            };
+
+            _client = _factory.CreateClient(clientOptions);
         }
 
         [Fact(DisplayName = "Obter status 200 ao buscar 5 ações magic formula (Usuário não logado)")]
@@ -58,9 +72,8 @@ namespace M4.WebApi.Tests
         public async Task DadoQueObter5MagicFormulaFoiChamado_QuandoOUsuarioEstiverDeslogado_DeveRetornar5PrimeirosRegistros()
         {
             // Arrange
-            var client = _factory.CreateClient();
             // Act
-            var response = await client.GetAsync("https://localhost/api/acoes/obter-5-magic-formula");
+            var response = await _client.GetAsync("acoes/obter-5-magic-formula");
             var body = await response.Content.ReadAsStringAsync();
             var registros = JsonSerializer.Deserialize<IEnumerable<AcaoClassificacao>>(body);
             // Assert
@@ -73,9 +86,8 @@ namespace M4.WebApi.Tests
         public async Task DadoQueObterTodasMagicFormulaFoiChamado_QuandoOUsuarioEstiverDeslogado_DeveRetornarOStatus401()
         {
             // Arrange
-            var client = _factory.CreateClient();
             // Act
-            var response = await client.GetAsync("https://localhost/api/acoes/obter-todas-magic-formula");
+            var response = await _client.GetAsync("acoes/obter-todas-magic-formula");
             var body = await response.Content.ReadAsStringAsync();
             // Assert
             Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
@@ -83,18 +95,53 @@ namespace M4.WebApi.Tests
         }
 
         [Fact(DisplayName = "Obter status 204 ao tentar cadastrar usuário com dados válidos")]
-        [Trait("Integração", "Ações")]
+        [Trait("Integração", "Usuario")]
         public async Task DadosQueOCadastroDeUsuariosFoiChamado_QuandoConterDadosValidos_DeveCadastrarUsuarioEEnviarEmailDeConfirmacao()
         {
             // Arrange
-            var usuario = new UsuarioCadastro { Email = "demo@demo.com.br", Senha = "Demo@666", SenhaConfirmacao = "Demo@666", Nome = "Diego", Sobrenome = "Romário"  };
-            var client = _factory.CreateClient();
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
             // Act
-            var response = await client.PostAsJsonAsync("https://localhost/api/usuario/cadastrar", usuario);
+            HttpResponseMessage response = await CadastrarUsuario();
             // Assert
             Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+        }
+
+        private async Task<HttpResponseMessage> CadastrarUsuario()
+        {
+            var usuario = new UsuarioCadastro { Email = "demo@demo.com.br", Senha = "Demo@666", SenhaConfirmacao = "Demo@666", Nome = "Diego", Sobrenome = "Romário" };
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            var response = await _client.PostAsJsonAsync("https://localhost/api/usuario/cadastrar", usuario);
+            return response;
+        }
+
+        private async Task<UsuarioRespostaLogin> LogarUsuario()
+        {
+            var usuarioLogin = new UsuarioLogin
+            {
+                Email = "demo@demo.com.br",
+                Senha = "Demo@666"
+            };
+
+            var response = await _client.PostAsJsonAsync("usuario/entrar", usuarioLogin);
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            var usuarioResponse = JsonSerializer.Deserialize<UsuarioRespostaLogin>(body, new JsonSerializerOptions() { 
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            });
+            return usuarioResponse;
+        }
+
+        [Fact(DisplayName = "Obter dados de usuário cadastrados (token) ao se logar com dados válidos")]
+        [Trait("Integração", "Usuario")]
+        public async Task DadosQueExisteOUsuarioCadastrado_QuandoOLoginForRequisitado_DeveRetornarOsDadosDoMesmo()
+        {
+            // Arrange
+            await CadastrarUsuario();
+            // Act
+            var response = await LogarUsuario();
+            // Assert
+            Assert.NotNull(response.AccessToken);
+            Assert.True(response.AccessToken.Length > 20);
         }
     }
 }
