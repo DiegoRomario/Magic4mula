ee22f591c9d0cd72766f8250ca47c412c18964d8
Diego Romário <diego.romario@outlook.com>
2021-08-31 21:47:39 -0300

    Iniciando implementação de function para envio de e-mail

---
+++ src/M4.Functions/EmailSender/Core/EmailCreator.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Net.Mail;
+
+namespace EmailSender.Core
+{
+    public class EmailCreator
+    {
+        private readonly EmailConfiguration _emailConfig;
+        private readonly ILogger<EmailCreator> _logger;
+
+        public EmailCreator(IOptions<EmailConfiguration> emailConfig, ILogger<EmailCreator> logger)
+        {
+            _emailConfig = emailConfig.Value;
+            _logger = logger;
+        }
+        public bool SendEmail(string subject, string message, string to)
+        {
+            var from = _emailConfig.From;
+
+            MailMessage mailMessage = new MailMessage();
+            SmtpClient smtpClient = new SmtpClient();
+            try
+            {
+                MailAddress fromAddress = new MailAddress(from);
+                mailMessage.From = fromAddress;
+                foreach (var item in to.Split(";"))
+                {
+                    mailMessage.To.Add(item);
+                }
+
+                mailMessage.Subject = subject;
+                mailMessage.IsBodyHtml = true;
+                mailMessage.Body = message;
+                smtpClient.Host = _emailConfig.SmtpServer;
+                smtpClient.Port = _emailConfig.Port;
+                smtpClient.EnableSsl = true;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.Credentials =
+                    new System.Net.NetworkCredential(_emailConfig.UserName, _emailConfig.Password);
+                smtpClient.Send(mailMessage);
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Ocorreu um erro ao tentar enviar e-mail para {to}: " + ex.Message);
+                throw;
+            }
+
+        }
+    }
+}
---
+++ src/M4.Functions/EmailSender/Core/EmailQueue.cs
@@ -0,0 +1,52 @@
+﻿using M4.Infrastructure.Data.Context;
+using M4.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+
+namespace EmailSender.Core
+{
+    public class EmailQueue
+    {
+        private readonly DbContextOptions<MagicFormulaDbContext> _optionsBuilder;
+        private readonly EmailCreator _emailCreator;
+        private readonly ILogger<EmailQueue> _logger;
+
+        public EmailQueue(EmailCreator emailCreator, ILogger<EmailQueue> logger)
+        {
+            _optionsBuilder = new DbContextOptions<MagicFormulaDbContext>();
+            _emailCreator = emailCreator;
+            _logger = logger;
+        }
+
+        public void DequeueEmail()
+        {
+            using var dataBase = new MagicFormulaDbContext(_optionsBuilder);
+
+            var emailRequests = dataBase.Set<EmailSolicitacao>()
+                .Where(s => !s.Enviado).AsNoTracking().ToList();
+
+            foreach (var request in emailRequests)
+            {
+                try
+                {
+                    _emailCreator.SendEmail(request.Titulo, request.Mensagem,
+                        request.Destinatarios);
+                    request.DataEnvio = DateTime.Now;
+                    request.Enviado = true;
+                    dataBase.EmailSolicitacao.Update(request);
+                    dataBase.SaveChanges();
+
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"Ocorreu um erro ao tentar enviar o item {request.Id} da fila de e-mail: " + ex.Message);
+                    throw;
+                }
+
+            }
+        }
+
+    }
+}
